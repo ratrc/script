316955b206015fa5f436917a5059dc34543f0db3
paulofurtado <paulofurtadojunior@gmail.com>
2025-03-20 10:02:20 -0300

    teste

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('teste');
+document.addEventListener("DOMContentLoaded", function() {
+    const scripts = document.getElementsByTagName("script");
+    let idd = '';
+
+    for (let script of scripts) {
+        if (script.src.includes("script.js")) {
+            const url = new URL(script.src);
+            idd = url.searchParams.get('ratId');
+
+            if (idd) {
+                console.log('idd', idd);
+                break;
+            }
+        }
+    }
+});
